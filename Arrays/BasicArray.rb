bdce312801d86f2f7ea5004e2f7867bdc7d1195a
pw <pw@Devops.com>
2019-02-19 02:05:46 -0600

    Arrays

---
+++ Arrays/BasicArray.rb
@@ -0,0 +1,5 @@
+friends = Array["Prasad" ,"Ganesh","Sagar"]
+puts friends
+puts friends[1]
+friends[1]="kiran"
+puts friends
---
+++ Arrays/NewArray.rb
@@ -0,0 +1,7 @@
+Friends = Array.new
+Friends[0] ="amar"
+Friends[4] ="Anthoney"
+puts Friends.length()
+puts Friends.include? "amar"
+
+puts Friends.reverse()
